bf2de328b13ab58b0704c0695c5571e4570dcd28
Dhaval Shekhada <dhavalshekhada@gmail.com>
2024-08-20 20:59:57 +0200

    updated test

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -5,7 +5,6 @@ import { app } from '../../app';
 import { Order } from '../../models/order';
 import { stripe } from '../../stripe';
 import { Payment } from '../../models/payment';
-require('dotenv').config()
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
